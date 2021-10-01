de34a77446d328beebaeb712769d81e63b3faf42
SarpBalci0 <91756282+SarpBalci0@users.noreply.github.com>
2021-10-01 21:53:46 +0300

    Add files via upload

---
+++ not-hesaplama.py
@@ -0,0 +1,22 @@
+print('Not Hesaplama Uygulaması')
+
+ad=input('Öğrenci Adı-Soyadı Giriniz: ')
+
+not1=float(input('Öğrencinin İlk Notunu Giriniz: '))
+ 
+
+not2=float(input('Öğrencinin İkinci Notunu Giriniz: '))
+ 
+
+not3=float(input('Öğrencinin Üçüncü Notunu Giriniz: '))
+ 
+
+ortalama=not1+not2+not3/3
+
+print('Öğrenci Adı Soyadı: ',ad)
+print('Not Ortalaması= ',ortalama)
+
+
+
+
+
